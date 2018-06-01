90f1291060951fffd203eebe7c9c2d226bb8d328
huangcw <229834707@qq.com>
2018-06-01 09:44:59 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/administrator/coolweather/db/Province.java
@@ -0,0 +1,35 @@
+package com.coolweather.administrator.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+
+    private String provinceName;
+
+    private int privinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getPrivinceCode() {
+        return privinceCode;
+    }
+
+    public void setPrivinceCode(int privinceCode) {
+        this.privinceCode = privinceCode;
+    }
+}
